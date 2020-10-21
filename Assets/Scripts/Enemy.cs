4d1f2d7dca5fe5af710cdc0f19dec00c5cdbad9a
say106 <say016@usask.ca>
2020-10-20 22:17:58 -0600

    added basic skull enemy with working healthbar

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
+
+//base enemy class
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int health;
+    public bool isMousedOver = false;
+    
+    //below are used to instantiate and manage the healthbar
+    public Canvas healthCanvasPrefab;
+    protected Canvas healthCanvas;
+    private Healthbar healthbar;
+
+    
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        healthCanvas = Instantiate(healthCanvasPrefab, transform.position, transform.rotation, gameObject.transform);
+        health = maxHealth;
+        healthbar = healthCanvas.GetComponentInChildren<Healthbar>();
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+		{
+            Destroy(gameObject);
+		}
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        healthbar.SetHealth(health);
+    }
+
+    private void OnMouseDown()
+    {
+        /* For testing healthbar
+        if (Input.GetMouseButtonDown(0))
+        {
+            TakeDamage(10);
+        }
+        */
+    }
+
+    private void OnMouseOver()
+	{
+        isMousedOver = true;  
+	}
+
+	private void OnMouseExit()
+	{
+        isMousedOver = false;
+	}
+}
---
+++ Assets/Scripts/Healthbar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Manages the value of the healthbar as well as its size when moused over
+public class Healthbar : MonoBehaviour
+{
+    public Slider slider;
+    private Transform canvasTransform;
+    private Enemy enemy;
+
+    private void Start()
+    {
+        canvasTransform = GetComponentInParent<RectTransform>(); //RectTransform of the UI Canvas
+        enemy = GetComponentInParent<Enemy>();
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+    }
+
+    public void SetHealth(int health)
+    {
+        float previous = slider.value;
+        slider.value = health;
+    }
+
+    private void Update()
+    {
+        //Enlarge healthbar when enemy moused over
+        if (enemy.isMousedOver)
+        {
+            canvasTransform.localScale = Vector3.Lerp(canvasTransform.localScale, new Vector3(1.5f, 1.5f, 1.5f), 10 * Time.deltaTime);
+        }
+        else
+        {
+            canvasTransform.localScale = Vector3.Lerp(canvasTransform.localScale, new Vector3(1f, 1f, 1f), 10 * Time.deltaTime);
+        }
+    }
+
+}
