f035661b50a63f96206a3161da3f9c12f762f277
Ben Ablass (bja379) <bja379@usask.ca>
2020-10-22 19:50:33 -0600

    Imported some stuff for Textmesh Pro and modified weapons

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -7,4 +7,12 @@ public class ItemData : ScriptableObject
     public string itemName;            // name of this item
     public int value;              // How much could this be sold for
     public List<ItemData> recipe;  // List of items that could be put together to make this item
+    public Sprite scroll;          // What scroll should this item be displayed on
+    public string description;     // description of the item
+
+    public (GameObject, GameObject) DisplayDescription()
+    {
+        GameObject text = new GameObject();
+
+    }
 }
