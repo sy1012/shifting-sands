a9b86ecc6f1a07a804ea2475c32d9120bd2c02a7
bigd2413 <boyes.duncan@gmail.com>
2020-11-08 13:20:51 -0800

    Major reworking of Room Picker architecture to enable image to tilemap generation so I donthave to make a prefab for each room 4 different rotations.

---
+++ Assets/DoorOrientation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOrientation : MonoBehaviour
+{
+    public enum Heading { N, E, S, W };
+    public Heading heading;
+    private void Awake()
+    {
+        switch (heading)
+        {
+            case Heading.N:
+                transform.up = Vector3.up;
+                break;
+            case Heading.E:
+                transform.up = Vector3.right;
+                break;
+            case Heading.S:
+                transform.up = Vector3.down;
+                Debug.Log("Flip door" + transform.up);
+                break;
+            case Heading.W:
+                transform.up = Vector3.left;
+                break;
+        }
+    }
+    private void Start()
+    {
+        switch (heading)
+        {
+            case Heading.N:
+                transform.up = Vector3.up;
+                break;
+            case Heading.E:
+                transform.up = Vector3.right;
+                break;
+            case Heading.S:
+                transform.up = Vector3.down;
+                Debug.Log("Flip door" + transform.up);
+                break;
+            case Heading.W:
+                transform.up = Vector3.left;
+                break;
+        }
+    }
+}
