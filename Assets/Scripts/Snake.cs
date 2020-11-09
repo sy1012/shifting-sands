e3c0592840c27880f5b5cec24c63aa4d8d294560
say106 <say016@usask.ca>
2020-11-08 18:59:40 -0600

    created prefabs for each new enemy type with AI included

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,48 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Skull enemy
+public class Snake : Enemy
+{
+
+    [SerializeField]
+    private Room room;
+    [SerializeField]
+    private AIDestinationSetter destination;
+    [SerializeField]
+    private Transform Player;
+    private PlayerStateMachine psm;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        damageSpeed = 1;
+        damage = 10;
+        //move healthbar to a more suitable position
+        healthCanvas.transform.position = transform.position + new Vector3(0, 0.7f, 0);
+        psm = Player.GetComponent<PlayerStateMachine>();
+        
+    }
+
+    private void Update()
+    {
+        if (room == null)
+        {
+            throw new System.Exception("The Snake enemy:" + transform.name + "'s room is Null. Set Snake's room.");
+        }
+        if (psm == null)
+        {
+            throw new System.Exception("The Snake enemy:" + transform.name + "'s target player is Null");
+        }
+        if(room.Equals(psm.GetRoom()))
+        {
+            destination.target = Player;
+        }
+    }
+
+    
+
+}
