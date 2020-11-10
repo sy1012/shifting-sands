d90caf1216d1d5c005536992514a3006c905e1f4
bigd2413 <boyes.duncan@gmail.com>
2020-11-10 01:41:21 -0800

    Started on Encounter generation

---
+++ Assets/Scripts/RoomFilling/EncounterFactory.cs
@@ -0,0 +1,33 @@
+﻿namespace RoomFilling
+{
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class EncounterFactory:MonoBehaviour
+    {
+        public int difficulty = 1;
+        [SerializeField]
+        public List<Encounter> EncounterPool;
+        public List<MonoBehaviour> GetEncounter()
+        {
+            int j = UnityEngine.Random.Range(0, EncounterPool.Count);
+            List<MonoBehaviour> pool = new List<MonoBehaviour>();
+            foreach (var mob in EncounterPool[j].EncounterDescription)
+            {
+                int number = mob.diceRoll.Roll();
+                for (int i = 0; i < number; i++)
+                {
+                    pool.Add(Instantiate(mob.prefab));
+                }
+            }
+            return pool;
+        }
+    }
+    [Serializable]
+    public class Encounter
+    {
+        [SerializeField]
+        public List<GenericPoolItem<MonoBehaviour>> EncounterDescription;
+    }
+}
---
+++ Assets/Scripts/RoomFilling/GenericFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RoomFilling
+{
+    public class GenericFactory:MonoBehaviour
+    {
+        [SerializeField]
+        private MonoBehaviour prefab;
+        public MonoBehaviour GetNewInstance()
+        {
+            return Instantiate(prefab);
+        }
+    }
+    [SerializeField]
+
+    public class EntranceFactory : GenericFactory
+    {
+
+    }
+
+}
