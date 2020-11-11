88f3452c6a7e2db4458fd997606d84e0082af349
bigd2413 <boyes.duncan@gmail.com>
2020-11-11 14:10:10 -0800

    Fixed enemy pathfinding bug. The Pathfinder was making its graph before the dungeon was generated because it does that on awake, so I attached a mono that gets the Pathfinder to scan after 60 frames. Graph now has the walls as untraversable

--- Assets/RefreshPathfinder.cs
+++ Assets/RefreshPathfinder.cs
@@ -2,21 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using System;
 
 public class RefreshPathfinder : MonoBehaviour
 {
+    int tick = 0;
+    int max = 60;
     // Start is called before the first frame update
     void Awake()
     {
-        EventManager.onDungeonGenerated += RefreshPathfindingGraph;
+        tick = 0;
     }
 
+    private void Update()
+    {
+        //Refresh after 60 frames once. Only once
+        if (tick > max)
+        {
+            RefreshPathfindingGraph(EventArgs.Empty);
+        }
+        tick++;
+    }
 
     private void OnDestroy()
     {
-        EventManager.onDungeonGenerated -= RefreshPathfindingGraph;
         
     }
+    //FORCES THE PATHFINDER TO REFRESH
     private void RefreshPathfindingGraph(System.EventArgs e)
     {
         AstarPath astarPath = GetComponent<AstarPath>();
