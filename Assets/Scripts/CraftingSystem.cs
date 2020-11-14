815f8da434bae7e7c8509092a4b858986f240f1a
Duncan Boyes <boyes.duncan@gmail.com>
2020-11-13 18:17:13 -0800

    Beginings of crafting

---
+++ Assets/Scripts/CraftingSystem.cs
@@ -0,0 +1,31 @@
+﻿public class CraftingSystem
+{
+    public const int maxIngredients = 3;
+    private Item[] items; 
+    public CraftingSystem()
+    {
+        items = new Item[maxIngredients];
+    }
+    private bool IsEmpty(int index) { return items[index] == null; }
+    private void SetItem(Item item, int index)
+    {
+        items[index] = item;
+    }
+    private void RemoveItem(Item item, int index)
+    {
+        SetItem(null, index);
+    }
+    private bool TryAddItem(Item item, int index)
+    {
+        if (IsEmpty(index))
+        {
+            SetItem(item, index);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
---
+++ Assets/UICraftingSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICraftingSystem : MonoBehaviour
+{
+    [SerializeField] private Transform prefabItem;
+    private Transform[] inputSlots;
+    private Transform outputSlot;
+    private Transform itemContainer;
+
+    private void Awake()
+    {
+        Transform gridContainer = transform.Find("gridContainer");
+        Transform itemContainer = transform.Find("itemContainer");
+
+        inputSlots = new Transform[CraftingSystem.maxIngredients];
+
+        for (int i = 0; i < inputSlots.Length; i++)
+        {
+            inputSlots[i] = gridContainer.Find("input_"+i);
+        }
+        outputSlot = gridContainer.Find("output");
+    }
+    private void CreateItem(int index, Item item)
+    {
+        Transform itemTransform = Instantiate(prefabItem, itemContainer);
+        RectTransform rectTransform = itemTransform.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = inputSlots[index].GetComponent<RectTransform>().anchoredPosition;
+        
+    }
+}
