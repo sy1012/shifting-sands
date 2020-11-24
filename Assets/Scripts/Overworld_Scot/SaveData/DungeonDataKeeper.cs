690ff36eb82af79c2762b46f8186d80c2abbdd37
Duncan Boyes <boyes.duncan@gmail.com>
2020-11-24 15:30:41 -0800

    Made into singlton the dungeon data keeper

--- Assets/Scripts/Overworld_Scot/SaveData/DungeonDataKeeper.cs
+++ Assets/Scripts/Overworld_Scot/SaveData/DungeonDataKeeper.cs
@@ -5,11 +5,35 @@ using UnityEngine;
 
 public class DungeonDataKeeper : MonoBehaviour
 {
+
+    public static DungeonDataKeeper instance = null;
+
+
     public bool beatLastDungeon = false;
     public float dungeonDistance = 0;
     public DungeonVariant dungeonVariant = DungeonVariant.tiny;
+
     private void Awake()
     {
-        DontDestroyOnLoad(this.gameObject);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    public static DungeonDataKeeper getInstance()
+    {
+        if (instance == null)
+        {
+            var newDDK = new GameObject("DungeonDataKeeper");
+            newDDK.AddComponent<DungeonDataKeeper>();
+        }
+        return instance;
     }
 }
