928fe1d53ad52d025a3986d640d0c01df7aa3678
say106 <say016@usask.ca>
2020-12-04 20:51:41 -0600

    added shitty camel walk animation to overworld

---
+++ Assets/camelAnimationController.cs
@@ -0,0 +1,70 @@
+﻿using GraphGrammars;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camelAnimationController : MonoBehaviour
+{
+    public Follower camel;
+    public Animator animator;
+    private Transform destination;
+    private Vector3 prevPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevPosition = camel.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        destination = ((OasisNode)camel.path[0]).getOasis().transform;
+
+        Vector2 direction = destination.position - camel.transform.position;
+        Debug.Log(direction.x);
+        if (camel.next != null)
+        {
+            if (direction == Vector2.zero || Vector2.Distance(camel.transform.position, camel.next.transform.position) <= camel.followDistance)
+            {
+                animator.Play("idle");
+            }
+            else
+            {
+                animator.Play("walk");
+                if (direction.x < 0)
+                {
+                    transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
+                }
+                else
+                {
+                    transform.localScale = new Vector3(-0.15f, 0.15f, 0.15f);
+                }
+
+            }
+        }
+        else
+        {
+            if (direction == Vector2.zero || Vector2.Distance(camel.transform.position, camel.leader.transform.position) <= camel.followDistance)
+            {
+                animator.Play("idle");
+            }
+            else
+            {
+                animator.Play("walk");
+                if (direction.x < 0)
+                {
+                    transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+                }
+                else
+                {
+                    transform.localScale = new Vector3(-0.25f, 0.25f, 0.25f);
+                }
+
+            }
+        }
+
+
+        prevPosition = camel.transform.position;
+        
+    }
+}
