f252906604d4913a947d8868efd3abf0eee6e0ba
say106 <say016@usask.ca>
2020-12-05 19:42:06 -0600

    fixed weapon not moving with player

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    PlayerStateMachine psm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        psm = FindObjectOfType<PlayerStateMachine>();
+    }
+
+    void LateUpdate()
+    {
+        if(psm == null)
+        {
+            psm = FindObjectOfType<PlayerStateMachine>();
+        }
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = psm.transform.position + (mouse - psm.transform.position).normalized * 3;
+    }
+}
