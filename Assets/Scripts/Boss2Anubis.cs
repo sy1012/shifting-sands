fad4a67a41bc8a126a44bf5ed0cfb04ae99da33e
Duncan Boyes <boyes.duncan@gmail.com>
2020-12-07 19:26:14 -0800

    New line in boss name

--- Assets/Scripts/Boss2Anubis.cs
+++ Assets/Scripts/Boss2Anubis.cs
@@ -58,7 +58,7 @@ public class Boss2Anubis : Enemy
         bossText = new GameObject("boss text");
         bossText.transform.SetParent(this.transform);
         bossText.AddComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
-        bossText.AddComponent<TextMeshProUGUI>().text = "Anubis The Wise";
+        bossText.AddComponent<TextMeshProUGUI>().text = "\nAnubis The Wise";
         bossText.GetComponent<TextMeshProUGUI>().fontSize = 25;
         bossText.GetComponent<TextMeshProUGUI>().alignment = TextAlignmentOptions.TopGeoAligned;
         bossText.GetComponent<RectTransform>().position = Camera.main.ViewportToScreenPoint(new Vector2(.5f, .895f));
